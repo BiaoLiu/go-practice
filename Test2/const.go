0dcd103b6d5c4aafc11be753ffbd17199ac57997
LBI <452381072@qq.com>
2018-03-01 22:38:56 +0800

    目录更改

---
+++ Test2/const.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+const a = "1"
+const b = "2"
+const (
+	e,f=1,2
+	c="3"
+	d="4"
+)
+
+func main() {
+	var a int=1
+	var b=2
+	c:=3
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println("const")
+
+}
